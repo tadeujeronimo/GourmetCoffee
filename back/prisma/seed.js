7190016375b128cd6f78ef5531cbc92106300d3f
Ynã Ponte <ynaqp@alu.ufc.br>
2026-06-13 17:00:04 -0300

    chore(back): Criação do usuário administrador idempotente utilizando upsert

--- back/prisma/seed.js
+++ back/prisma/seed.js
@@ -2,14 +2,16 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.administrador.create({
-    data: {
+  await prisma.administrador.upsert({
+    where: { email: 'admin@admin.com' },
+    update: {},
+    create: {
       email: 'admin@admin.com',
       senha: '123',
     },
   });
 
-  console.log('Admin criado com sucesso');
+  console.log('Admin criado/verificado com sucesso');
 }
 
 main()
